43c0671e31b8d1047b75111b4dca616b7724de55
swdeveloperabhi <kumar.abhishek79781@gmail.com>
2026-03-13 09:03:04 +0530

    Room availability check feature

--- src/UseCase1HotelBookingApp.java
+++
@@ -1,6 +0,0 @@
-public class UseCase1HotelBookingApp{
-    public static void main(String[] args){
-        System.out.println("Welcome to Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
-    }
-}
---
+++ src/UseCase2HotelBookingApp.java
@@ -0,0 +1,87 @@
+abstract class Room {
+
+    private int beds;
+    private int size;
+    private double price;
+
+    public Room(int beds, int size, double price) {
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract String getRoomType();
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+
+    public String getRoomType() {
+        return "Single Room";
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+
+    public String getRoomType() {
+        return "Double Room";
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+
+    public String getRoomType() {
+        return "Suite Room";
+    }
+}
+public class UseCase2HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization\n");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        printRoom(single, singleAvailable);
+        printRoom(doubleRoom, doubleAvailable);
+        printRoom(suite, suiteAvailable);
+    }
+
+    public static void printRoom(Room room, int available) {
+        System.out.println(room.getRoomType() + ":");
+        System.out.println("Beds: " + room.getBeds());
+        System.out.println("Size: " + room.getSize() + " sqft");
+        System.out.println("Price per night: " + room.getPrice());
+        System.out.println("Available: " + available);
+        System.out.println();
+    }
+}
