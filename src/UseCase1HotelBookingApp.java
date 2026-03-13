60671e3b093a9ce7de33bc794ab0e830e2e84c50
swdeveloperabhi <kumar.abhishek79781@gmail.com>
2026-03-13 08:53:07 +0530

    Application entry and Welcome message

--- src/UseCase1HotelBookingApp.java
+++ src/UseCase1HotelBookingApp.java
@@ -1,5 +1,6 @@
 public class UseCase1HotelBookingApp{
     public static void main(String[] args){
-
+        System.out.println("Welcome to Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
     }
 }
